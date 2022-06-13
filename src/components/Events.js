ac435312e70e346f68300b9206bc3e90154900f7
maryums <maryum@gmail.com>
2022-06-12 22:22:29 -0400

    .env added

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import daygrid from "@fullcalendar/daygrid";
-
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
 
+
+
 const Events = () => {
+
+    console.log(process.env.REACT_APP_API_KEY)
 
 
     return (
@@ -17,7 +20,7 @@ const Events = () => {
                 //     center: 'title',
                 //     right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 // }}
-                googleCalendarApiKey={API_KEY}
+                googleCalendarApiKey={process.env.REACT_APP_API_KEY}
                 events={{ googleCalendarId: 'v91g62mmvh6v2o925vvdj74edg@group.calendar.google.com' }}
 
             />
